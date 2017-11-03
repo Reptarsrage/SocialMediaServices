808e94c8b2749b91decf416d035f45820bd25a3b
Justin Robb <reptarsrage@gmail.com>
2017-11-03 14:43:57 -0700

    Simplified build scripts and nuget

--- build.bat
+++ build.bat
@@ -1,10 +1,7 @@
 @echo off
 
-echo Download FAKE and place it in packages/FAKE
-nuget install FAKE -OutputDirectory packages -ExcludeVersion
-
-echo Download NuGet and place it in packages/nuget
-nuget install NuGet.CommandLine -OutputDirectory packages -ExcludeVersion
-
-echo Run FAKE against the build script with args %*
-packages\FAKE\tools\FAKE.exe %* 
+dotnet clean
+dotnet restore --packages "./packages"
+dotnet build --configuration=Release --no-restore
+dotnet test "./test/SocialMediaServices.Tests.Unit/SocialMediaServices.Tests.Unit.csproj" --configuration=Release --no-restore --no-build
+dotnet pack "./src/SocialMediaServices/SocialMediaServices.csproj" --configuration=Release --no-restore --no-build --output "../../artifacts"
